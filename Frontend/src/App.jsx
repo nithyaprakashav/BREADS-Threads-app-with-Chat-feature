b4278dc3804e40b3c9f2be7d016c24c9f3b535ec
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-18 19:11:32 +0530

    App setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@chakra-ui/button"
+
+
+function App() {
+ 
+  return (
+    <>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ChakraProvider} from "@chakra-ui/provider"
+import {mode} from "@chakra-ui/theme-tools"
+import {extendTheme} from '@chakra-ui/react'
+import { ColorModeScript } from '@chakra-ui/react'
+
+
+
+
+const styles = {
+  global:(props) =>({
+    body:{
+      color:mode('gray.800', 'whiteAlpha.900')(props),
+      bg:mode('gray.100','#101010')(props),
+    }
+  }) 
+}
+
+const config = {
+  intialColorMode : "dark",
+  useSystemColorMode:false
+}
+
+const colors = {
+  gray:{
+    light:'#616161',
+    dark:'#1e1e1e'
+  }
+}
+
+const theme = extendTheme({config , styles, colors})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+    
+  </React.StrictMode>,
+)
