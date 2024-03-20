e69cf15e4a9c00175819996793bab18a739428dc
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-21 01:23:31 +0530

    Add icons and assets

--- Frontend/src/components/UserPost.jsx
+++ Frontend/src/components/UserPost.jsx
@@ -1,9 +1,11 @@
 import { Avatar, Box, Flex ,Text , Image} from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
 import { Link } from "react-router-dom";
-
+import Icons from "./Icons";
+import { useState } from "react";
 
 const UserPost = () => {
+    const [liked , setLiked] = useState(false)
     return ( 
         <Link to={"/nithyaprakash/post/1"}>
             <Flex gap={3} mb={4} py={5}>
@@ -64,6 +66,29 @@ const UserPost = () => {
                             <BsThreeDots/>
                         </Flex>
                     </Flex>
+
+                    <Text fontSize={"sm"}>Hey guys!! wassup!</Text>
+                    <Box
+                    borderRadius={6}
+                    overflow={"hidden"}
+                    border={"1px solid"}
+                    borderColor={"gray.light"}
+                    >
+                        <Image src="np-avatar.jpg" w={"full"}></Image>
+                    </Box>
+                    <Flex
+                    gap={3}
+                    my={1}
+                    >
+                        <Icons liked={liked} setLiked={setLiked} />
+                    </Flex>
+
+                    <Flex gap={2} alignItems={"center"}>
+                        <Text color={"gray.light"} fontSize={"sm"}>22 replies</Text>
+                        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"} fontSize={"sm"} ></Box>
+                        <Text color={"gray.light"} fontSize={"sm"}>3012 likes</Text>
+                    </Flex>
+
                 </Flex>
             </Flex>
         </Link>
