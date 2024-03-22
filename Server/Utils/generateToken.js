4703e3cab7e93f709e853d15d9ec10b89aec366e
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-22 17:18:20 +0530

    Create generateToken function

---
+++ Server/Utils/generateToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+
+const generateToken = (userId , res) => {
+    const token = jwt.sign({userId} , process.env.SECRET, {
+        expiresIn:"10d"
+    })
+
+    res.cookie("token"  ,token , {
+        httpOnly: true,
+        maxAge: 10 * 24 * 60 * 60 * 1000,
+        sameSite: "strict"
+    })
+
+    return token
+}
+
+export default generateToken
