95a17bd7efa77556865be4c965ce90fa2a24d3c2
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-22 18:35:11 +0530

    Add verifytoken to routes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { signupUser } from "../controllers/userController.js"
+import { signupUser , loginUser ,logoutUser ,followUnfollowUser } from "../controllers/userController.js"
 
 
 const router = express.Router()
 
 router.post("/signup" , signupUser)
+router.post("/login" , loginUser)
+router.post("/logout" , logoutUser)
+router.post("/follow/:id" ,verifyToken, followUnfollowUser)
 
 export default router
