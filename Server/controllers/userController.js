8ec2d6ec106bb34688efa9bf6f08eb6c12d863db
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-22 16:46:46 +0530

    Add userSignup function

---
+++ Server/controllers/userController.js
@@ -0,0 +1,41 @@
+import User from "../models/UserModel.js"
+import bcrypt from 'bcrypt'
+
+const signupUser = async (req, res) => {
+    try {
+        const {name , username , email , password} = req.body
+        const user = await User.findOne({$or: [{email},{username}]})
+        if(user){
+            return res.status(400).json({message: "User already exists"}) 
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password , salt)
+
+        const newUser = new User({
+            name , 
+            email , 
+            username , 
+            password: hashedPassword,
+        })
+
+        await newUser.save()
+
+        if(newUser){
+            res.status(201).json({
+                _id : newUser._id,
+                name:newUser.name,
+                email: newUser.email,
+                username: newUser.username,
+            })
+        }else{
+            res.status(400).json({message: "Invalid user data"})
+        }
+
+    } catch (err) {
+        res.status(500).json({message : err.message})
+        console.log("Error in signupUser :",err.message)
+    }
+}
+
+
+export {signupUser}
