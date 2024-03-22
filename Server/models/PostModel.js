49dc3ef280716253a9799105b94c3a1411f1a6cb
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-22 13:59:23 +0530

    Add PostModelSchema

---
+++ Server/models/PostModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    postedBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text:{
+        type: String,
+        maxLength: 500,
+    },
+    img:{
+        type:String,
+        default:"",
+    },
+    likes:{
+        type:Number,
+        default:0
+    },
+    replies:[
+        {
+            userId:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'User',
+                required: true
+            },
+            text:{
+                type:String ,
+                required: true,
+            },
+            userProfilePic:{
+                type: String , 
+            },
+            userName:{
+                type: String
+            }
+        }
+    ]
+},
+{
+    timeStamps: true
+})
+
+const Post = mongoose.model('Post' , postSchema)
+
+export default Post
