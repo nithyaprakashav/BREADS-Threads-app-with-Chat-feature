866a38c06fc96a58457c3494b80352b9121802ec
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-22 16:47:50 +0530

    Add routes to server

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./mongoDB/connectDB.js"
+import cookieParser from "cookie-parser"
+import userRoutes from "../Server/routes/userRoutes.js"
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 connectDB()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+//Routes
+
+app.use("/api/users" , userRoutes)
 
 app.listen(3001 , () => console.log(`Server running on port ${PORT} successfully`))
