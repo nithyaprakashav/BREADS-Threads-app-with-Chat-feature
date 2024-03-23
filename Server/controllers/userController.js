6a53d02701738525bdf6a4ce7f5f7ca67d026d91
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-23 15:06:09 +0530

    Add updateprofile function

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -81,7 +81,7 @@ const followUnfollowUser = async (req, res) => {
         const userToActOn = await User.findById(id)
         const currUser = await User.findById(req.user._id)
 
-        if(id === req.user._id) return res.status(400).json({mssg: "you cannot follow or unfollow yourself"})
+        if(id === req.user._id.toString()) return res.status(400).json({mssg: "you cannot follow or unfollow yourself"})
 
         if(!userToActOn || !currUser) return res.status(500).json({error: "User not found"})
 
@@ -102,4 +102,42 @@ const followUnfollowUser = async (req, res) => {
     }
 }
 
-export {signupUser , loginUser , logoutUser , followUnfollowUser}
+const updateUser = async (req, res) => {
+    try {
+        const {name , email , username , password , profilePic, bio}= req.body
+        const userId = req.user._id
+        let user = await User.findById(userId)
+        if(!user){
+            return res.status(400).json({message: "User not found"})
+        }
+
+        if(req.params.id !== userId.toString()) return res.status(400).json({message:"You cannot update other user's profile"})
+
+        if(password){
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword =await bcrypt.hash(password , salt)
+            user.password = hashedPassword
+        } 
+        user.name = name || user.name
+        user.email = email || user.email
+        user.username = username || user.username
+        user.profilePic = profilePic || user.profilePic
+        user.bio = bio || user.bio
+        user = await user.save()
+        res.status(200).json({message: "Profile updated successfully" , user})
+
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
+
+const getUserProfile = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
+export {signupUser , loginUser , logoutUser , followUnfollowUser , updateUser , getUserProfile}
