e23910406feb9fe15e0e4ef812a3e6814be07d93
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-23 15:06:51 +0530

    Add visit profile route

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signupUser , loginUser ,logoutUser ,followUnfollowUser } from "../controllers/userController.js"
+import { signupUser , loginUser ,logoutUser ,followUnfollowUser , updateUser, getUserProfile } from "../controllers/userController.js"
 import verifyToken from "../middlewares/verifyToken.js"
 
 const router = express.Router()
@@ -8,5 +8,7 @@ router.post("/signup" , signupUser)
 router.post("/login" , loginUser)
 router.post("/logout" , logoutUser)
 router.post("/follow/:id" ,verifyToken, followUnfollowUser)
+router.post("/update/:id" ,verifyToken, updateUser)
+router.get("/profile/:id" , getUserProfile)
 
 export default router
