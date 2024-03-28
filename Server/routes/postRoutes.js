96b2373e2a683104c20db2b012be47840e9d12e3
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-28 15:06:27 +0530

    Add getpost and deletePost route

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { createPost } from '../controllers/postController.js'
+import { createPost , getPost ,deletePost} from '../controllers/postController.js'
 import verifyToken from '../middlewares/verifyToken.js'
 
 
 const router = express.Router()
 
+router.get("/:id" ,verifyToken, getPost)
 router.post ("/create" ,verifyToken, createPost)
+router.delete ("/delete/:id" ,verifyToken, deletePost)
 
 
 export default router
