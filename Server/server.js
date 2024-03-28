f68458e70eb23baa60cc592601829f4e82c8907a
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-28 12:20:49 +0530

    Add Post Route

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./mongoDB/connectDB.js"
 import cookieParser from "cookie-parser"
 import userRoutes from "../Server/routes/userRoutes.js"
+import postRoutes from "./routes/postRoutes.js"
 
 dotenv.config()
 const app = express()
@@ -17,5 +18,6 @@ app.use(cookieParser())
 //Routes
 
 app.use("/api/users" , userRoutes)
+app.use("/api/posts" , postRoutes)
 
 app.listen(3001 , () => console.log(`Server running on port ${PORT} successfully`))
