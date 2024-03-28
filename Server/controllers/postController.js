bc80989d803da4bcb4399dc5506070cd69aaef94
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-28 15:08:12 +0530

    Add getPost and deletePost function

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -20,3 +20,33 @@ export const createPost = async ( req , res) => {
         res.status(500).json({mssg : err.message})
     }
 }
+
+export const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            return res.status(404).json({message:"Post not found"})
+        }
+        res.status(200).json({post})
+
+    } catch (err) {
+        res.status(400).json({mssg:err.message})
+    }
+}
+
+export const deletePost = async (req , res) => {
+    try {
+        const post =await Post.findById(req.params.id)
+        if(!post){
+            return res.status(404).json({mssg:"Post not found"})
+        }
+        if(post.postedBy.toString() !== req.user._id.toString()){
+            return res.status(401).json({mssg:"Unauthorized to delete the post"})
+        }
+
+        await Post.findByIdAndDelete(req.params.id)
+        res.status(200).json({mssg:"Post deleted successfully"})
+    } catch (err) {
+        res.status(400).json({mssg:err.message})
+    }
+}
