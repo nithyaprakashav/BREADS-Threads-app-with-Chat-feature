b4eb4e5ec578da53529484958694551f3303262e
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-29 18:35:37 +0530

    Add authpage and Homepage

---
+++ Frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+import  SignupCard  from "../components/SignupCard";
+import  LoginCard  from "../components/LoginCard";
+
+const AuthPage = () => {
+    return ( 
+        <>
+            <LoginCard/>
+        </>
+     );
+}
+ 
+export default AuthPage
