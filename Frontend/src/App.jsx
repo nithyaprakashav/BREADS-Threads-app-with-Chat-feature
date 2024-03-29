5ef19b1841537ff4d48ac269b65c0718aa9a9e54
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-29 18:33:40 +0530

    Add home and auth pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import {Routes , Route} from "react-router-dom"
 import PostPage from "./pages/PostPage"
 import NavBar from "./components/NavBar"
 import UserPage from "./pages/UserPage"
+import HomePage from "./pages/HomePage"
+import AuthPage from "./pages/AuthPage"
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     <Container maxW="620px">
       <NavBar/>
       <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:pid" element={<PostPage/>} />
       </Routes>
