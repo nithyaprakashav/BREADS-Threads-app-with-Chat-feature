1df4058f5ac43818c38b77b25964a792c7223869
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-29 13:43:25 +0530

    Add commentPost route

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost , getPost ,deletePost , likeUnlikePost} from '../controllers/postController.js'
+import { createPost , getPost ,deletePost , likeUnlikePost , commentPost} from '../controllers/postController.js'
 import verifyToken from '../middlewares/verifyToken.js'
 
 
@@ -9,6 +9,7 @@ router.get("/:id" ,verifyToken, getPost)
 router.post ("/create" ,verifyToken, createPost)
 router.delete ("/delete/:id" ,verifyToken, deletePost)
 router.post("/like/:id" , verifyToken , likeUnlikePost )
+router.post("/comment/:id" , verifyToken , commentPost )
 
 
 export default router
