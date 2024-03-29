b33a03f1959d9da1ea551de1cb82efccdf2f6d39
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-29 14:29:21 +0530

    Add getFeedPosts route

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createPost , getPost ,deletePost , likeUnlikePost , commentPost} from '../controllers/postController.js'
+import { createPost , getPost ,deletePost , likeUnlikePost , commentPost , getFeedPosts} from '../controllers/postController.js'
 import verifyToken from '../middlewares/verifyToken.js'
 
 
 const router = express.Router()
 
+router.get("/feed" , verifyToken , getFeedPosts)
 router.get("/:id" ,verifyToken, getPost)
 router.post ("/create" ,verifyToken, createPost)
 router.delete ("/delete/:id" ,verifyToken, deletePost)
