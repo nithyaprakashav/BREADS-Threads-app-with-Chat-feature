1b634ac9a9822bddb9c00df7fcf1c25302d164b7
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-29 14:29:45 +0530

    Add getFeedPosts function

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -99,3 +99,19 @@ export const commentPost = async(req , res) => {
         res.status(500).json({error:err.message})
     }
 }
+
+export const getFeedPosts = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const user= await User.findById(userId)
+        if(!user){
+            return res.status(404).json({mssg:"User not found"})
+        }
+        const following = user.following
+        const feedPosts  = await Post.find({postedBy:{$in:following}}).sort({createdAt:-1})
+
+        res.status(200).json({feedPosts})
+    } catch (err) {
+        res.status(500).json({error:err.message})
+    }
+}
