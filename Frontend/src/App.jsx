fa8d1ce77d7386257cc927d575ad975bd89e82cd
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-03-31 18:05:51 +0530

    Add userAtom to App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import { Button } from "@chakra-ui/button"
 import { Container } from "@chakra-ui/react"
-import {Routes , Route} from "react-router-dom"
+import {Routes , Route, Navigate} from "react-router-dom"
 import PostPage from "./pages/PostPage"
 import NavBar from "./components/NavBar"
 import UserPage from "./pages/UserPage"
 import HomePage from "./pages/HomePage"
 import AuthPage from "./pages/AuthPage"
+import { useRecoilValue } from "recoil"
+import userAtom from "./atoms/userAtom"
 
 
 function App() {
  
+  const user = useRecoilValue(userAtom)
+
   return (
     <Container maxW="620px">
       <NavBar/>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/auth" element={<AuthPage/>}/>
+        <Route path="/" element={user ? <HomePage/> : <Navigate to="/auth"/>}/>
+        <Route path="/auth" element={!user ?<AuthPage/> : <Navigate to="/"/>}/>
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:pid" element={<PostPage/>} />
       </Routes>
