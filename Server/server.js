40b37f1e20f3aaa030d8093cf145fd9c91960419
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-04 11:40:57 +0530

    install cloudinary

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,18 @@ import connectDB from "./mongoDB/connectDB.js"
 import cookieParser from "cookie-parser"
 import userRoutes from "../Server/routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
+import {v2 as cloudinary} from "cloudinary"
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 connectDB()
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
