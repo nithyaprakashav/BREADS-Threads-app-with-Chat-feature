1777a810716837cbfe05c9e620e6c046679c69bd
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-04 10:08:21 +0530

    Add hook to select image

---
+++ Frontend/src/hooks/useImgPreview.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import useShowToast from "../hooks/useShowToast"
+const useImgPreview = () => {
+
+    const [imageUrl , setImageUrl] = useState(null)
+    const showToast = useShowToast()
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0]
+        if(file && file.type.startsWith("image/")){
+            const fileReader = new FileReader()
+
+            fileReader.onloadend = () => {
+                setImageUrl(fileReader.result)
+            }
+
+            fileReader.readAsDataURL(file)
+        }else{
+            showToast("Error" , "Please select image files only" , "error")
+            setImageUrl(null)
+        }
+        console.log(imageUrl)
+    }
+
+    return {handleImageChange , imageUrl};
+}
+ 
+export default useImgPreview;
