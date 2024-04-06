70297253e2a22f0ea988e78adb5a20f933a9605d
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-06 14:43:40 +0530

    useCallback and getUser

--- Frontend/src/pages/UserPage.jsx
+++ Frontend/src/pages/UserPage.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import UserNavbar from "../components/UserNavbar";
 import UserPost from "../components/UserPost";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const UserPage = () => {
+
+    const [user , setUser] = useState(null)
+    const showToast = useShowToast()
+    const {username } = useParams()
+    useEffect(() => {
+        const getUser = async() => {
+            try {
+                const response= await fetch(`/api/users/profile/${username}`)
+                const data = await response.json()
+                if(data.error){
+                    showToast("Error" , data.error , "error")
+                    return
+                }
+                setUser(data)
+                console.log(data)
+            } catch (err) {
+                showToast("Error" , err , "error")
+            }
+        }
+        getUser()
+    },[username , showToast])
     return ( 
         <>
-            <UserNavbar/>
+            <UserNavbar user={username}/>
 
             <UserPost likes={2001} replies={304} postImg={"/np-avatar.jpg"} postTitle={"Hey guys! wassup!!"} />
             <UserPost likes={3225} replies={287} postImg={"/me-khachith-pavan.jpg"} postTitle={"Group of three!"}/>
