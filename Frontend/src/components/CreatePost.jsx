9cdbbdc609b0a9ffcfcd0716d3a4114a4b4029f2
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-07 12:29:08 +0530

    Add SelectImage and AddText function

--- Frontend/src/components/CreatePost.jsx
+++ Frontend/src/components/CreatePost.jsx
@@ -1,20 +1,34 @@
-import { Button, FormControl, Textarea, useColorModeValue , Text, Input } from "@chakra-ui/react";
+import { Button, FormControl, Textarea, useColorModeValue , Text, Input, Flex  , Image, CloseButton} from "@chakra-ui/react";
 import {AddIcon} from "@chakra-ui/icons"
 import { Modal , ModalOverlay , ModalContent , ModalHeader , ModalCloseButton , ModalBody , ModalFooter } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import useImgPreview from "../hooks/useImgPreview";
 import { BsFillImageFill } from "react-icons/bs";
-
+const MAX_CHAR = 500
 
 
 const CreatePost = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [postText , setPostText] = useState('')
-    const {handleImageChange} = useImgPreview()
+    const {handleImageChange , imageUrl , setImageUrl} = useImgPreview()
     const imageRef = useRef(null)
+    const [remainingChar , setRemainingChar] = useState(MAX_CHAR)
 
-    const handleTextChange = () => {
+    const handleTextChange = (e) => {
+        const inputText = e.target.value
+
+        if(inputText.length > MAX_CHAR){
+            const truncatedText = inputText.slice(0, MAX_CHAR)
+            setPostText(truncatedText)
+            setRemainingChar(0)
+        }else{
+            setPostText(inputText)
+            setRemainingChar(MAX_CHAR - inputText.length)
+        }
+    }
+
+    const handlePost = async() => {
 
     }
 
@@ -44,7 +58,7 @@ const CreatePost = () => {
                             value={postText}
                         />
                         <Text fontSize={"xs"} fontWeight={"bold"} textAlign={"right"} m={1} color={"gray.800"} >
-                            500/500
+                            {remainingChar}/{MAX_CHAR}
                         </Text>
                         <Input
                             type="file"
@@ -54,11 +68,26 @@ const CreatePost = () => {
                         />
                         <BsFillImageFill style={{marginLeft:"5px" ,cursor:"pointer" , size:"16" }} onClick={() => imageRef.current.click()} />
                     </FormControl>
+
+                    {imageUrl && (
+                        <Flex mt={5} w={"full"} position={"relative"} >
+                            <Image src={imageUrl} alt="Selected Image" />
+                            <CloseButton onClick={()=>{
+                                setImageUrl("")
+                            }}
+                            background={"gray.800"}
+                            position={"absolute"}
+                            top={2}
+                            right={2}
+                            />
+                        </Flex>
+                    )}
+
                 </ModalBody>
 
                 <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                    Close
+                    <Button colorScheme='blue' mr={3} onClick={handlePost}>
+                    Post
                     </Button>
                 </ModalFooter>
                 </ModalContent>
