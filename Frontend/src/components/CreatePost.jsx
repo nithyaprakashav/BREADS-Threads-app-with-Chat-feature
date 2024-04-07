869529f65abb14f8c51b7f80d023e07ebb0c7134
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-07 11:29:02 +0530

    Design createPost Modal

--- Frontend/src/components/CreatePost.jsx
+++ Frontend/src/components/CreatePost.jsx
@@ -1,10 +1,22 @@
-import { Button, FormControl, Textarea, useColorModeValue } from "@chakra-ui/react";
+import { Button, FormControl, Textarea, useColorModeValue , Text, Input } from "@chakra-ui/react";
 import {AddIcon} from "@chakra-ui/icons"
 import { Modal , ModalOverlay , ModalContent , ModalHeader , ModalCloseButton , ModalBody , ModalFooter } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import useImgPreview from "../hooks/useImgPreview";
+import { BsFillImageFill } from "react-icons/bs";
+
 
 
 const CreatePost = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [postText , setPostText] = useState('')
+    const {handleImageChange} = useImgPreview()
+    const imageRef = useRef(null)
+
+    const handleTextChange = () => {
+
+    }
 
     return ( 
         <>
@@ -14,6 +26,7 @@ const CreatePost = () => {
                 right={10}
                 leftIcon={<AddIcon/>}
                 bg={useColorModeValue("gray.300" , "gray.dark")}
+                onClick={onOpen}
             >
                 Post
             </Button>
@@ -28,7 +41,18 @@ const CreatePost = () => {
                         <Textarea
                             placeholder="Post content goes here..."
                             onChange={handleTextChange}
+                            value={postText}
                         />
+                        <Text fontSize={"xs"} fontWeight={"bold"} textAlign={"right"} m={1} color={"gray.800"} >
+                            500/500
+                        </Text>
+                        <Input
+                            type="file"
+                            hidden
+                            ref={imageRef}
+                            onChange={handleImageChange}
+                        />
+                        <BsFillImageFill style={{marginLeft:"5px" ,cursor:"pointer" , size:"16" }} onClick={() => imageRef.current.click()} />
                     </FormControl>
                 </ModalBody>
 
