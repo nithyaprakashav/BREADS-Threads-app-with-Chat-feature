f0a458219c222ea8f9494a1f3fc461a5348a9bc2
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-10 11:13:38 +0530

    Add loading spinner

--- Frontend/src/pages/UserPage.jsx
+++ Frontend/src/pages/UserPage.jsx
@@ -3,12 +3,15 @@ import UserNavbar from "../components/UserNavbar";
 import UserPost from "../components/UserPost";
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
 
     const [user , setUser] = useState(null)
     const showToast = useShowToast()
     const {username } = useParams()
+    const [isLoading , setIsLoading] = useState(true)
+
     useEffect(() => {
         const getUser = async() => {
             try {
@@ -22,12 +25,27 @@ const UserPage = () => {
                 console.log(data)
             } catch (err) {
                 showToast("Error" , err , "error")
+            }finally{
+                setIsLoading(false)
             }
         }
         getUser()
     },[username])
 
-    if(!user) return null
+    if(!user && isLoading){
+        return (
+            <Flex justifyContent={"center"}>
+                <Spinner size={"xl"}  />
+            </Flex>
+            
+        )
+    }
+
+    if(!user && !isLoading){
+        return (
+            <h1>User not found</h1>
+        )
+    }
 
     return ( 
         <>
