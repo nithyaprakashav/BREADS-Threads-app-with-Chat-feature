1c9c777226c465b80b776709bae77280b43978c3
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-06 18:23:03 +0530

    Create getUserPosts function

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -125,3 +125,15 @@ export const getFeedPosts = async (req, res) => {
         res.status(500).json({error:err.message})
     }
 }
+
+export const getUserPosts = async (req, res)=>{
+    const{username} = req.params
+    try {
+        const user = await User.findOne({username})
+        if(!user) return res.status(404).json({error:"User not found"})
+        const posts = await Post.find({postedBy: user._id}).sort({createdAt:-1})
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
