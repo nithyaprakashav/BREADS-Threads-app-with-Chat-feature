620548a9932bb5ed54bd7f3ff59e71956a147271
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-07 18:50:25 +0530

    add delete image

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -46,12 +46,16 @@ export const getPost = async (req, res) => {
 
 export const deletePost = async (req , res) => {
     try {
-        const post =await Post.findById(req.params.id)
+        const postId = req.params
+        const post =await Post.findById(postId)
         if(!post){
             return res.status(404).json({error:"Post not found"})
         }
         if(post.postedBy.toString() !== req.user._id.toString()){
             return res.status(401).json({error:"Unauthorized to delete the post"})
+        }
+        if(post.img){
+            await cloudinary.uploader.destroy(post.img.split("/").pop().split(".")[0])
         }
 
         await Post.findByIdAndDelete(req.params.id)
