3a9fda2d3fc9c74d817dd95ebd0bb2f8ce61efd5
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-10 18:24:35 +0530

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <Container maxW="620px">
       <NavBar isLoggedIn={user} />
       <Routes>
-        <Route path="https://breads-threads-app-with-chat-feature-backend.vercel.app/" element={user ? <HomePage/> : <Navigate to="/auth"/>}/>
+        <Route path="https://breads-threads-app-with-chat-feature-backend.vercel.app/" element={<AuthPage/>}/>
         <Route path="https://breads-threads-app-with-chat-feature-backend.vercel.app/auth" element={!user ?<AuthPage/> : <Navigate to="/"/>}/>
         <Route path="https://breads-threads-app-with-chat-feature-backend.vercel.app/update" element={user ?<UpdateProfile/> : <Navigate to="/auth"/>}/>
         <Route path="https://breads-threads-app-with-chat-feature-backend.vercel.app/:username" element={user ? (
