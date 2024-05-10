6d9f429fd4e9ebe2214ff0b2e9e26bf28b135edf
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-10 18:06:43 +0530

    Vercel deploy try

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/postRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
 
 dotenv.config()
+const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 5000
 connectDB()
@@ -16,6 +17,14 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
+
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended: true}))
