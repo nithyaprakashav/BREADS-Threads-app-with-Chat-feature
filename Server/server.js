8102e0dcd7f9b6d24f1a22cea23bd5561b9ee91d
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-11 15:38:24 +0530

    step 2

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ import postRoutes from "./routes/postRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
 
 dotenv.config()
-// const cors = require("cors")
+const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 5000
 connectDB()
@@ -18,13 +18,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-// app.use(cors(
-//     {
-//         origin:["https://breads-threads-app-with-chat-feature-frontend.vercel.app"],
-//         methods:["POST","GET"],
-//         credentials:true
-//     }
-// ))
+app.use(cors(
+    {
+        origin:["https://breads-threads-app-with-chat-feature-frontend.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended: true}))
