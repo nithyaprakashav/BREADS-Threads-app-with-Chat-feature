8a711f4d24a9e5c65fa813626cacb79739a84919
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-12 20:26:10 +0530

    Revert "changes for deploy on render"
    This reverts commit db5f90d4b6760afd53769abba542051bc8e54367.

--- Server/server.js
+++ Server/server.js
@@ -5,28 +5,28 @@ import cookieParser from "cookie-parser"
 import userRoutes from "../Server/routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
-import path from 'path'
-import { fileURLToPath } from "url"
+
 dotenv.config()
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 =======
 // const cors = require("cors")
 >>>>>>> parent of 8102e0d (step 2)
+=======
+const cors = require("cors")
+>>>>>>> parent of db5f90d (changes for deploy on render)
 const app = express()
 const PORT = process.env.PORT || 5000
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
 connectDB()
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 
@@ -40,6 +40,15 @@ cloudinary.config({
 //     }
 // ))
 >>>>>>> parent of 8102e0d (step 2)
+=======
+app.use(cors(
+    {
+        origin:["https://breads-threads-app-with-chat-feature-frontend.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
+>>>>>>> parent of db5f90d (changes for deploy on render)
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended: true}))
