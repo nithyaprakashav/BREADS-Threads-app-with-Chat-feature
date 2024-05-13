20f5f5160737b3b72aa72ed728ac933f04fdbfe7
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-13 18:37:39 +0530

    checking with different server endpoint

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import dotenv from "dotenv"
+// dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
+    port:4000,
     proxy:{
       "/api":{
-        target:"https://breads-threads-app-with-chat-feature-1.onrender.com",
+        target:"http://localhost:5000",
         changeOrigin: true ,
         secure: false
       }
