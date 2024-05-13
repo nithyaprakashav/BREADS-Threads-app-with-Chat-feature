03c98f597bfd8bd15ae668e47235aacac7b604d5
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-13 18:44:42 +0530

    reset target route

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port:4000,
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://breads-threads-app-with-chat-feature-1.onrender.com",
         changeOrigin: true ,
         secure: false
       }
