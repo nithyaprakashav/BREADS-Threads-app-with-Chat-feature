fd8bd3a48d0a6a0f4c8fb09691d2600f27dee927
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-17 11:35:04 +0530

    Design chat layout

--- Frontend/src/components/Conversations.jsx
+++ Frontend/src/components/Conversations.jsx
@@ -1,6 +1,43 @@
+import { Avatar, AvatarBadge, Flex, Image, Stack, Text, WrapItem, useColorModeValue } from "@chakra-ui/react";
+
 const Conversations = () => {
     return ( 
-        <div>Conversations</div>
+        
+        <Flex
+            gap={4}
+            alignItems={'center'}
+            p={'1'}
+            _hover={{
+                cursor:'pointer',
+                bg:useColorModeValue("gray.600","gray.dark"),
+                color:"white"
+            }}
+            borderRadius={"md"}
+        >
+            <WrapItem>
+                <Avatar size={{
+                    base:"xs",
+                    sm:"sm",
+                    md:"md"
+                }} src="https://bit.ly/broken.link">
+
+                    <AvatarBadge boxSize={"1em"} bg="green.500" />
+                </Avatar>    
+            </WrapItem>
+
+            <Stack direction={"column"} fontSize={"sm"} >
+                <Text fontWeight={"700"}
+                    display={'flex'}
+                    alignItems={"center"}
+                >
+                    tsunade <Image src="/verified.png" w={4} h={4} ml={1} />
+                </Text>
+                <Text fontSize={"xs"} display={"flex"} alignItems={"center"} gap={1} >
+                    Random message
+                </Text>
+            </Stack>
+
+        </Flex>
      );
 }
  
