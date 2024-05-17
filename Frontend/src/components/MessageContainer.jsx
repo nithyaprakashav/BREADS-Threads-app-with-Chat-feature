8e46d222115b816f6d2771bf128ef5dd080ea194
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-17 13:23:44 +0530

    Update

--- Frontend/src/components/MessageContainer.jsx
+++ Frontend/src/components/MessageContainer.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Divider, Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
+import { Avatar, Divider, Flex, Image, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react";
 
 const MessageContainer = () => {
     return ( 
@@ -15,6 +15,26 @@ const MessageContainer = () => {
             </Flex>
 
             <Divider/>
+
+            <Flex flexDir={"column"} gap={4} my={4} height={"400px"} overflowY={"scroll"} >
+                {true && (
+                    [0,1,2,3,4].map((_,i)=>(
+                        <Flex key={i} gap={2} alignItems={"center"} p={1} borderRadius={"md"} alignSelf={i % 2 == 0 ? "flex-start" : "flex-end"}>
+                            {i % 2 == 0 && (
+                                <SkeletonCircle size={7} />
+                            )}
+                            <Flex flexDirection={"column"} gap={2} >
+                                <Skeleton h={"8px"} w={"250px"} />
+                                <Skeleton h={"8px"} w={"250px"} />
+                                <Skeleton h={"8px"} w={"250px"} />
+                            </Flex>
+                            {i % 2 !== 0 && (
+                                <SkeletonCircle size={7} />
+                            )}
+                        </Flex>
+                    ))
+                )}
+            </Flex>
 
         </Flex>
         
