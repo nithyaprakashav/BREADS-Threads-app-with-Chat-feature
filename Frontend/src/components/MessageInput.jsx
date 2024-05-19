b4c6e857b584c2b1075c86f06f1514793ecf2fe8
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-19 18:22:12 +0530

    Add MessageInput component

---
+++ Frontend/src/components/MessageInput.jsx
@@ -0,0 +1,18 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import {IoSendSharp} from "react-icons/io5"
+
+
+const MessageInput = () => {
+    return ( 
+        <form>
+            <InputGroup>
+                <Input w={"full"}  placeholder="Type a message" />
+                    <InputRightElement>
+                    <IoSendSharp/>
+                    </InputRightElement>
+            </InputGroup> 
+        </form>
+     );
+};
+ 
+export default MessageInput;
