ab4967ebdb3012c9596973a4f8e576f298c48f5c
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-21 13:23:38 +0530

    Add getMessages route new

--- Server/routes/messageRoutes.js
+++ Server/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import verifyToken from '../middlewares/verifyToken.js'
-import { sendMessage } from '../controllers/messageController.js'
+import { sendMessage,getMessages } from '../controllers/messageController.js'
 
 const router = express.Router()
 
 router.post('/',verifyToken,sendMessage)
+router.get('/:otherUserId',verifyToken,getMessages)
 
 export default router
