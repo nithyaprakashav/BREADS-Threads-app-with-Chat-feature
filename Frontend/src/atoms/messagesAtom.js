4606b6b83aef39bbe8646ceaf32fa2ed9cd1c0c5
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-22 14:07:40 +0530

    create global state

--- Frontend/src/atoms/messagesAtom.js
+++ Frontend/src/atoms/messagesAtom.js
@@ -3,4 +3,15 @@ import {atom} from 'recoil'
 export const conversationsAtom = atom({
     key:"conversationsAtom",
     default:[]
+})
+
+
+export const selectedConversationAtom = atom({
+    key:"selectedConversationAtom",
+    default:{
+        _id:"",
+        userId:"",
+        username:"",
+        userProfilePic:""
+    }
 }) 
