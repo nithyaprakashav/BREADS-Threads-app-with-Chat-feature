6b507e76ba9978da7282369738d9865649222b63
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-22 14:08:13 +0530

    Fix

--- Frontend/src/components/Message.jsx
+++ Frontend/src/components/Message.jsx
@@ -1,6 +1,9 @@
-import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Flex, Text, useColorMode } from "@chakra-ui/react";
 
-const Message = ({userMessage,}) => {
+const Message = ({userMessage}) => {
+
+    const colorMode = useColorMode()
+
     return ( 
         <>
         {userMessage ? (
@@ -8,7 +11,7 @@ const Message = ({userMessage,}) => {
             gap={2}
             alignSelf={"flex-end"}
             >  
-                <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"} >Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+                <Text maxW={"350px"} bg=  {"blue.400"} p={1} borderRadius={"md"} >Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
                 <Avatar src="" w={7} h={7} />
             </Flex>
         ):(
@@ -17,7 +20,7 @@ const Message = ({userMessage,}) => {
             
             >  
                 <Avatar src="" w={7} h={7} />
-                <Text maxW={"350px"} bg={"gray.900"} p={1} borderRadius={"md"} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quae sit exercitationem numquam similique.</Text>
+                <Text maxW={"350px"} bg= {colorMode === "dark" ? "gray.900": "blue.100" } color={colorMode === 'dark' ? "black":""} p={1} borderRadius={"md"} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quae sit exercitationem numquam similique.</Text>
                 
             </Flex>
         )}
