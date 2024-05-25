4dba2d64ee07cc97199701d672f4501ed291473f
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-25 15:50:25 +0530

    Add scrolldown to latestText functionality and fix bugs

--- Frontend/src/components/MessageContainer.jsx
+++ Frontend/src/components/MessageContainer.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Divider, Flex, Image, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react";
 import Message from "./Message";
 import MessageInput from "./MessageInput";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom"
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
@@ -18,13 +18,18 @@ const MessageContainer = () => {
     const[message , setMessage] = useState([])
     const setConversations = useSetRecoilState(conversationsAtom)
     const {socket} = useSocket()
+    const latestMessageRef = useRef(null)
 
     useEffect(()=>{
         socket.on("newMessage",(message) => {
-            setMessage( prevMessage => [...prevMessage, message])
+            
+            if(selectedConversation._id === message.conversationId){
+                setMessage( prevMessage => [...prevMessage, message])
+            }
+
             setConversations( prevConvos => {
                 const updatedConversations = prevConvos.map((convo)=>{
-                    if(convo._id === selectedConversation._id){
+                    if(convo._id === message.conversationId){
                         return {
                             ...convo,
                             lastMessage:{
@@ -68,6 +73,10 @@ const MessageContainer = () => {
         getMessages()
     },[showToast,selectedConversation.userId])
 
+    useEffect(()=>{
+        latestMessageRef.current?.scrollIntoView({behaviour:"smooth"})
+    },[message])
+
 
     return ( 
         <Flex flex={70}
@@ -106,8 +115,12 @@ const MessageContainer = () => {
                 )}
 
                 {!isLoading && (
-                    message.map((message)=>(
-                        <Message key={message._id } message={message} userMessage={message.sender === currUser._id } />
+                    message.map((msg)=>(
+                        <Flex key={msg._id }direction={"column"}
+                            ref={message.length -1 === message.indexOf(msg) ? latestMessageRef : null}
+                        >
+                            <Message  message={msg} userMessage={msg.sender === currUser._id } />
+                        </Flex>
                     ))
                 )}
 
