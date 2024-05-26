0de0fcf3884da95d07569c9c88e49e8da4c7874b
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-26 15:51:09 +0530

    Update UI for Image Messages

--- Frontend/src/components/Message.jsx
+++ Frontend/src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text, useColorMode } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Image, Text, useColorMode } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import { selectedConversationAtom } from "../atoms/messagesAtom";
 import userAtom from "../atoms/userAtom";
@@ -9,7 +9,6 @@ const Message = ({userMessage, message}) => {
     const selectedConversation = useRecoilValue(selectedConversationAtom)
     const colorMode = useColorMode()
     const currUser = useRecoilValue(userAtom)
-
     return ( 
         <>
         {userMessage ? (
@@ -17,21 +16,47 @@ const Message = ({userMessage, message}) => {
             gap={2}
             alignSelf={"flex-end"}
             >  
+            {message.text && (
                 <Flex bg={"green.800"} maxW={"350px"} p={1} borderRadius={"md"} >
-                    <Text color={"white"} >{message.text}</Text>
-                    <Box alignSelf={"flex-end"} ml={1} color={message.seen? "blue.400" : ""} fontWeight={"bold"} >
-                        <BsCheck2All size={16} />
-                    </Box>
+                <Text color={"white"} >{message.text}</Text>
+                <Box alignSelf={"flex-end"} ml={1} color={message.seen? "blue.400" : ""} fontWeight={"bold"} >
+                    <BsCheck2All size={16} />
+                </Box>
+            </Flex>
+            )}
+
+            {message.img && (
+                <Flex mt={5} w={"200px"} >
+                    <Image 
+                        src="/Jiraiya.jpg"
+                        alt="Image as Message"
+                        borderRadius={4}
+                    />
                 </Flex>
+            )}
+                
                 <Avatar src={currUser.profilePic} w={7} h={7} />
             </Flex>
         ):(
             <Flex
             gap={2}
-            
             >  
                 <Avatar src={selectedConversation.userProfilePic} w={7} h={7} />
-                <Text maxW={"350px"} bg= {colorMode === "dark" ? "gray.900": "blue.100" } color={colorMode === 'dark' ? "black":""} p={1} borderRadius={"md"} >{message.text}</Text>
+
+                {message.text && (
+                    <Text maxW={"350px"} bg= {colorMode === "dark" ? "gray.900": "blue.100" } color={colorMode === 'dark' ? "black":""} p={1} borderRadius={"md"} >{message.text}</Text>
+                )}
+
+                {message.img && (
+                    <Flex mt={5} w={"200px"} >
+                        <Image 
+                            src=""
+                            alt="Image as Message"
+                            borderRadius={4}
+                        />
+                    </Flex>
+                )}
+                
                 
             </Flex>
         )}
