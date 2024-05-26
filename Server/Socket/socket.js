ad39cd3cc453a4c5da0f3ddc55270846e978e4d7
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-26 14:58:59 +0530

    Add seen feature to conversations too

--- Server/Socket/socket.js
+++ Server/Socket/socket.js
@@ -1,6 +1,8 @@
 import {Server} from 'socket.io'
 import http from 'http'
 import express from 'express'
+import Message from '../models/MessageModel.js'
+import Conversation from '../models/ConversationModel.js'
 
 const app = express()
 const server = http.createServer(app)
@@ -24,6 +26,21 @@ io.on('connection' , (socket) => {
 
     if(userId !== "undefined") userSocketMap[userId] = socket.id
     io.emit('getOnlineUsers' , Object.keys(userSocketMap))
+
+    socket.on("markMessagesAsSeen",async({conversationId,userId}) => {
+        try {
+            await Message.updateMany({conversationId:conversationId,seen:false},{
+                $set:{seen:true}
+            })
+            await Conversation.updateOne({_id:conversationId,seen:false},{
+                $set:{"lastMessage.seen":true}
+            })
+            io.to(userSocketMap[userId]).emit("messagesSeen",{conversationId})
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    
 
     socket.on("disconnect", ()=>{
         console.log("user disconnected :")
