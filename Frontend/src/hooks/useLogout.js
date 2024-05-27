5124f9a0d0512707c8f0102ff9bface63dcb7529
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-27 13:25:19 +0530

    Create custom hook to logout user

---
+++ Frontend/src/hooks/useLogout.js
@@ -0,0 +1,35 @@
+import {  useSetRecoilState } from "recoil";
+import useShowToast from "./useShowToast";
+import userAtom from "../atoms/userAtom";
+
+
+const useLogout = () => {
+	const setUser = useSetRecoilState(userAtom);
+	const showToast = useShowToast();
+
+	const logout = async () => {
+		try {
+			const res = await fetch("/api/users/logout", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data = await res.json();
+
+			if (data.error) {
+				showToast("Error", data.error, "error");
+				return;
+			}
+
+			localStorage.removeItem("userinfo");
+			setUser(null);
+		} catch (error) {
+			showToast("Error", error, "error");
+		}
+	};
+
+	return logout;
+};
+
+export default useLogout;
