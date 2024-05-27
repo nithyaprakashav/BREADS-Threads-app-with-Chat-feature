6d17168d4a56cdd7925ff09834ee211ef5ef8bf8
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-27 16:22:18 +0530

    socket url update fr deployment

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({children}) => {
     const user = useRecoilValue(userAtom)
 
     useEffect(()=>{
-        const socket = io('http://localhost:3001',{
+        const socket = io('/',{
             query:{
                 userId:user?._id
             }
