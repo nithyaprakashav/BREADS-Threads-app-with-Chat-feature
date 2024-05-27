30a48a77a81ccd2effee9c94cf851bcfba3221db
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-27 13:23:43 +0530

    Add new route to freeze account

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { signupUser , loginUser ,logoutUser ,followUnfollowUser,
-updateUser, getUserProfile,getSuggestedUsers } from "../controllers/userController.js"
+updateUser, getUserProfile,getSuggestedUsers,freezeAccount } from "../controllers/userController.js"
 import verifyToken from "../middlewares/verifyToken.js"
 
 const router = express.Router()
@@ -10,6 +10,7 @@ router.post("/login" , loginUser)
 router.post("/logout" , logoutUser)
 router.post("/follow/:id" ,verifyToken, followUnfollowUser)
 router.put("/update/:id" ,verifyToken, updateUser)
+router.put("/freeze" ,verifyToken, freezeAccount)
 router.get("/profile/:query" , getUserProfile)
 router.get("/suggested",verifyToken,getSuggestedUsers)
 
