d673c82592f453f55ef5609a6f65add2dd7d15f2
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-05-27 16:34:47 +0530

    Add cron.js

---
+++ Server/cron/cron.js
@@ -0,0 +1,21 @@
+import cron from "cron";
+import https from "https";
+
+const URL = "https://breads-threads-with-chatfeature.onrender.com";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+	https
+		.get(URL, (res) => {
+			if (res.statusCode === 200) {
+				console.log("GET request sent successfully");
+			} else {
+				console.log("GET request failed", res.statusCode);
+			}
+		})
+		.on("error", (e) => {
+			console.error("Error while sending request", e);
+		});
+});
+
+export default job;
+
